aca0b097e270969336039146f14e88f6e62e643b
tessamac2205 <tessmcduffie@gmail.com>
2017-06-21 06:00:39 +0300

    update js
    update for function

--- pong.js
+++ pong.js
@@ -1,8 +1,31 @@
 
 
-function divideBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 / num2;
+function pingPong(number) {  
+  var result = [];
+  for (var i = 1; i <= number; i++){
+    if ((i % 15) === 0){
+      result.push("Ping-Pong");
+    } else if ((i % 5) === 0){
+      result.push("Pong");
+    } else if ((i % 3) === 0){
+      result.push("Ping");
+    } else {
+      result.push(i)
+    }
+  }
+  
+  return result;   
+
 }
+
+$(document).ready(function() {
+  $("form#pingPongForm").submit(function(event) {
+    event.preventDefault();
+
+    var input = parseInt($("input#number").val());
+    var result = pingPong(input);
+    result.forEach(function(number) {
+      $("ul#result").append("<li>"+number+"</li>");
+    });
+  });
+});
